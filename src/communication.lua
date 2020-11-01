52ad92ebbdaf6794b8e12814fb2a87b4cecedf6b
joopon <joopon.jnw@gmail.com>
2020-11-01 22:19:54 +0100

    add communication.lua, read receiver file at setup

--- src/communication.lua
+++ src/communication.lua
@@ -1,8 +1,28 @@
---
--- Created by IntelliJ IDEA.
--- User: jawa
--- Date: 01.11.20
--- Time: 21:04
--- To change this template use File | Settings | File Templates.
---
+local component = require("component")
 
+local communication = {}
+
+local receiver_list = {}
+local PATH_CONFIG = "/etc/communication.cfg"
+
+function communication.setup()
+    local config = io.open(PATH_CONFIG, "r")
+    if not config then
+        print("error communication.setup(): no config at "..PATH_CONFIG)
+        return false
+    end
+
+    for line in config:lines() do
+        local match = string.gmatch(line, "[^=]+")
+        local rcv_name = match()
+        local rcv_addr = match()
+        print("receiver: "..rcv_name.." address: "..rcv_addr)
+        receiver_list[rcv_name] = rcv_addr
+    end
+    config:close()
+    return true
+end
+
+
+
+return communication
