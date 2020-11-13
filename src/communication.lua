aea38c71a8fa47e07e05dc72e072b6faf4ea9c58
joopon <joopon.jnw@gmail.com>
2020-11-13 23:09:48 +0100

    add send() and send_blocking_ack(), add port to communication config which is used to communicate with corresponding device

--- src/communication.lua
+++ src/communication.lua
@@ -1,9 +1,39 @@
 local component = require("component")
+local modem = component.modem
+local event = require("event")
+local serialization = require("serialization")
 
 local communication = {}
 
 local receiver_list = {}
 local PATH_CONFIG = "/etc/communication.cfg"
+
+-- message types:
+communication.ACK_MSG = "communication_ack_message"
+
+local function get_address_port(receiver)
+    local rcv = receiver_list[receiver]
+    if rcv == nil then
+        print("warning communication: couldn't find receiver '".. receiver.."'in receiver_list")
+        return nil
+    end
+    local rcv_address = rcv["address"]
+    local rcv_port = rcv["port"]
+    return rcv_address, rcv_port
+end
+
+local function send(rcv_address, rcv_port, message_type, message)
+    local msg = {
+        message_type = message_type,
+        message = message
+    }
+    local msg_string = serialization.serialize(msg)
+    if not modem.send(rcv_address, rcv_port, msg_string) then
+        print("error communication: failed to send message")
+        return false
+    end
+    return true
+end
 
 function communication.setup()
     local config = io.open(PATH_CONFIG, "r")
@@ -13,11 +43,21 @@ function communication.setup()
     end
 
     for line in config:lines() do
-        local match = string.gmatch(line, "[^=]+")
+        local match = string.gmatch(line, "[^=,]+")
         local rcv_name = match()
         local rcv_addr = match()
-        print("receiver: "..rcv_name..", address: "..rcv_addr)
-        receiver_list[rcv_name] = rcv_addr
+        local rcv_port = match()
+        if rcv_name == nil or rcv_addr == nil or rcv_port == nil or tonumber(rcv_port) == nil then
+            print("warning communication.setup(): found bad formatted line: "..line)
+        else
+            print("receiver: "..rcv_name..", address: "..rcv_addr..", port: "..rcv_port)
+            rcv_port = tonumber(rcv_port)
+            receiver_list[rcv_name] = {
+                address = rcv_addr,
+                port    = rcv_port
+            }
+            modem.open(rcv_port)
+        end
     end
     config:close()
     return true
@@ -29,6 +69,49 @@ function communication.list_receivers()
         table.insert(list, rcv_name)
     end
     return list
+end
+
+-- message_type: string
+-- message: any except functions
+-- returns true on success, false on failure
+function communication.send(receiver, message_type, message)
+    local rcv_address, rcv_port = get_address_port(receiver)
+    if rcv_address == nil then
+        return false
+    end
+
+    return send(rcv_address, rcv_port, message_type, message)
+end
+
+-- message_type: string
+-- message: any except functions
+-- timeout: optional, adds timeout [s] waiting for ack
+-- returns true on success, false on failure
+function communication.send_blocking_ack(receiver, message_type, message, timeout)
+    local rcv_address, rcv_port = get_address_port(receiver)
+    if rcv_address == nil then
+        return false
+    end
+    if not send(rcv_address, rcv_port, message_type, message) then
+        return false
+    end
+
+    local ack_msg_string, _
+    if timeout then
+        local ret
+        ret, _, _, _, _, ack_msg_string = event.pull(timeout, "modem_message", modem.address, rcv_address, rcv_port)
+        if ret == nil then -- timed out waiting for ack message
+            return false
+        end
+    else
+        _, _, _, _, _, ack_msg_string = event.pull("modem_message", modem.address, rcv_address, rcv_port)
+    end
+    local ack_msg = serialization.unserialize(ack_msg_string)
+    if not (ack_msg["message_type"] == communication.ACK_MSG) then
+        print("error communication.send(): expected ack message, received: "..ack_msg["message_type"])
+        return false
+    end
+    return true
 end
 
 return communication
