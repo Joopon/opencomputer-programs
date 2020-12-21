73bba01290d82914cdc20cff291138578b1fd447
joopon <joopon.jnw@gmail.com>
2020-12-21 01:54:06 +0100

    communication.lua: implement message_listener() and receive(), change send_with_ack to always use same timeout (doesn't really make sense to wait for ack forever)

--- src/communication.lua
+++ src/communication.lua
@@ -2,19 +2,27 @@ local component = require("component")
 local modem = component.modem
 local event = require("event")
 local serialization = require("serialization")
+local queue = require("queue")
 
 local communication = {}
 
 local device_list = {}
+local message_queue = queue.new(10)
+local ack_queue = queue.new(3)
 local PATH_CONFIG = "/etc/communication.cfg"
+local ACK_TIMEOUT = 10
 
 -- message types:
 communication.ACK_MSG = "communication_ack_message"
 
+-- event names:
+communication.MSG_EVENT = "received_message_event"
+communication.ACK_EVENT = "received_ack_event"
+
 local function get_address_port(device)
     local dev = device_list[device]
     if dev == nil then
-        print("warning communication: couldn't find device '"..device.."'in device_list")
+        print("warning communication: couldn't find device '"..device.."' in device_list")
         return nil
     end
     local dev_address = dev["address"]
@@ -22,10 +30,23 @@ local function get_address_port(device)
     return dev_address, dev_port
 end
 
-local function send(rcv_address, rcv_port, message_type, message)
+-- port: number
+-- returns device name on success, nil if device not found
+local function get_device(address, port)
+    for device, addr_port in pairs(device_list) do
+        if addr_port["address"] == address and addr_port["port"] == port then
+            return device
+        end
+    end
+    print("warning communication: couldn't find device for address '"..address.."' and port "..port.." in device_list")
+    return nil
+end
+
+local function send(rcv_address, rcv_port, request_ack, message_type, message)
     local msg = {
         message_type = message_type,
-        message = message
+        message = message,
+        request_ack = request_ack
     }
     local msg_string = serialization.serialize(msg)
     if not modem.send(rcv_address, rcv_port, msg_string) then
@@ -35,10 +56,47 @@ local function send(rcv_address, rcv_port, message_type, message)
     return true
 end
 
+local function message_listener(event_name, receiver_address, sender_address, port, distance, message_string)
+    local msg = serialization.unserialize(message_string)
+
+    local new_msg = {
+        receiver_address = receiver_address,
+        sender_address = sender_address,
+        port = port,
+        distance = distance,
+        message_type = msg.message_type,
+        message = msg.message
+    }
+    if msg.message_type == communication.ACK_MSG then
+        if not queue.push(ack_queue, new_msg) then
+            print("warning communication: lost an ack, ack_queue too small")
+            return
+        end
+        event.push(communication.ACK_EVENT)
+        return
+    end
+
+    if queue.is_full(message_queue) then
+        print("warning communication: lost a message, message_queue too small")
+        return
+    end
+
+    if msg.request_ack then
+        if not send(sender_address, port, false, communication.ACK_MSG, "ack") then
+            print("warning communication: failed to send ack. Received Message wasn't added to message queue")
+            return
+        end
+    end
+
+    assert(queue.push(message_queue, new_msg))
+    event.push(communication.MSG_EVENT)
+end
+
+
 function communication.setup()
     local config = io.open(PATH_CONFIG, "r")
     if not config then
-        print("error communication.setup(): no config at "..PATH_CONFIG)R
+        print("error communication.setup(): no config at "..PATH_CONFIG)
         return false
     end
 
@@ -60,6 +118,11 @@ function communication.setup()
         end
     end
     config:close()
+
+    if not event.listen("modem_message", message_listener) then
+        print("error communication.setup(): couldn't register message_listener")
+        return false
+    end
     return true
 end
 
@@ -81,39 +144,69 @@ function communication.send(receiver, message_type, message)
         return false
     end
 
-    return send(rcv_address, rcv_port, message_type, message)
+    return send(rcv_address, rcv_port, false, message_type, message)
 end
 
 -- receiver: string, device name
 -- message_type: string
 -- message: any except functions
--- timeout: optional, adds timeout [s] waiting for ack
 -- returns true on success, false on failure
-function communication.send_blocking_ack(receiver, message_type, message, timeout)
+function communication.send_with_ack(receiver, message_type, message)
     local rcv_address, rcv_port = get_address_port(receiver)
     if rcv_address == nil then
         return false
     end
-    if not send(rcv_address, rcv_port, message_type, message) then
+    if not send(rcv_address, rcv_port, true, message_type, message) then
         return false
     end
 
-    local ack_msg_string, _
-    if timeout then
-        local ret
-        ret, _, _, _, _, ack_msg_string = event.pull(timeout, "modem_message", modem.address, rcv_address, rcv_port)
-        if ret == nil then -- timed out waiting for ack message
-            return false
-        end
-    else
-        _, _, _, _, _, ack_msg_string = event.pull("modem_message", modem.address, rcv_address, rcv_port)
+    local event_type, _, _, _, _, ack_msg_string = event.pull(ACK_TIMEOUT, communication.ACK_EVENT)
+    if event_type == nil then -- timed out waiting for ack message
+        return false
     end
-    local ack_msg = serialization.unserialize(ack_msg_string)
-    if not (ack_msg["message_type"] == communication.ACK_MSG) then
-        print("error communication.send(): expected ack message, received: "..ack_msg["message_type"])
+    local ack_msg = queue.pop(ack_queue)
+    if ack_msg == nil then
+        print("error communication.send_with_ack: expected ack in ack_queue")
+        return false
+    end
+    if not (ack_msg.message_type == communication.ACK_MSG) then
+        print("error communication.send_with_ack(): expected ack message, received: "..ack_msg["message_type"])
+        return false
+    end
+    if not (ack_msg.sender_address == rcv_address and ack_msg.port == rcv_port) then
+        print("warning communication.send_with_ack: received ack from unexpected device: "..ack_msg.sender_address)
         return false
     end
     return true
+end
+
+-- timeout: optional, adds timeout [s] waiting for ack
+-- returns on success: device: string, distance: number, message_type: string, message: any except functions
+--         on fail: nil
+function communication.receive(timeout)
+    if queue.is_empty(message_queue) then
+        if timeout then
+            local event_type = event.pull(timeout, communication.MSG_EVENT)
+            if event_type == nil then
+                return nil
+            end
+        else
+            local event_type = event.pull(communication.MSG_EVENT)
+        end
+    end
+
+    local msg = queue.pop(message_queue)
+    if(msg == nil) then
+        print("error communication.receive(): expected message queue to hold message")
+        return nil
+    end
+
+    local device = get_device(msg.sender_address, msg.port)
+    if msg.message_type == nil or msg.message == nil or device == nil then
+        return nil
+    end
+
+    return device, msg.distance, msg.message_type, msg.message
 end
 
 return communication
